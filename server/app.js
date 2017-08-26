7ba312694437b245d765900dd8af1af45bbd981e
ravi kumar <marveric47@gmail.com>
2017-08-26 11:35:20 +0530

    first commit

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+require('./services/passport.js')
+
+const app = express();
+require('./routes/authRoutes.js')(app);
+
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+    }
+    else {
+    next();
+    }
+});
+
+app.listen(PORT, function () {
+  console.log('Express server is up on port '+PORT);
+});
